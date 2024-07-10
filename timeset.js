72415bb02947a0c48394201be320055c48e1c7d7
sudarshan1pass <sudarshanpandat@gmail.com>
2024-07-10 20:58:04 +0530

    Add files via upload

---
+++ timeset.js
@@ -0,0 +1,14 @@
+const time = document.getElementById("time");
+const set = document.getElementById("set");
+
+
+    setInterval(()=>{
+        const count=new Date();
+        time.innerHTML = count.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        time.style.fontSize='100px'
+        time.style.display="flex-wrap "
+        time.style.padding="10px"
+        time.style.margin="100px"
+
+ 
+    })
